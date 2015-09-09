520d2dd4928332f213f58415a2540963204dd46b
Maxwell <maximeleuger@gmail.com>
2015-09-09 14:48:49 +0200

    second step ionic tabs bis

---
+++ www/src/header/headerController.js
@@ -0,0 +1,9 @@
+"use strict";
+
+angular.module('places')
+	.controller('HeaderCtrl',function($rootScope,$scope,$state){
+		$rootScope.$on('$stateChangeSuccess',
+			function(event,toState,toParams,fromState,fromParams){
+				$scope.title = toState.data.title;
+			});
+	});
