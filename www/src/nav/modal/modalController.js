b3ffbe27909787e86ec6bcde1d3ef3137a19a860
Maxwell <maximeleuger@gmail.com>
2015-09-10 10:11:46 +0200

    modal non fonctionnelle

--- www/src/nav/modal/modalController.js
+++ www/src/nav/modal/modalController.js
@@ -1,11 +1,41 @@
 "use strict";
 
-angular.module('places')
-.controller('ModalCtrl',function($rootScope,$scope){
-	$scope.openModal = function(){
-		$scope.modal.show();
-	};
-	$scope.closeModal = function(){
-		
-	}
-})
+// angular.module('places')
+// .controller('ModalCtrl',function($scope,$ionicModal){
+// 	$scope.closeModal = function(){
+// 		$ionicModal.fromTemplateUrl('src/nav/modal/more-modal.html',{
+// 			scope: $scope,
+// 			animation: 'slide-in-up'
+// 		}).then(function(modal){
+// 			$scope.modal = modal.hide();
+// 		});
+// 	};
+// });
+
+// angular.module('places')
+// .controller('ModalCtrl', function($scope, $ionicModal) {
+//   $ionicModal.fromTemplateUrl('src/nav/modal/more-modal.html', {
+//     scope: $scope,
+//     animation: 'slide-in-up'
+//   }).then(function(modal) {
+//     $scope.modal = modal;
+//   });
+//   $scope.openModal = function() {
+//     $scope.modal.show();
+//   };
+//   $scope.closeModal = function() {
+//     $scope.modal.hide();
+//   };
+//   //Cleanup the modal when we're done with it!
+//   $scope.$on('$destroy', function() {
+//     $scope.modal.remove();
+//   });
+//   // Execute action on hide modal
+//   $scope.$on('modal.hidden', function() {
+//     // Execute action
+//   });
+//   // Execute action on remove modal
+//   $scope.$on('modal.removed', function() {
+//     // Execute action
+//   });
+// });
--- www/src/nav/tabs/tabs.controller.js
+++ www/src/nav/tabs/tabs.controller.js
@@ -1,17 +1,42 @@
 "use strict";
 
-angular.module('places')
-.controller('TabsCtrl',function($scope,$ionicModal){
-	$scope.openModal = function(){
+// angular.module('places')
+// .controller('TabsCtrl',function($scope,$ionicModal){
+// 	$scope.openModal = function(){
+// 		$ionicModal.fromTemplateUrl('src/nav/modal/more-modal.html',{
+// 			scope: $scope,
+// 			animation: 'slide-in-up'
+// 		}).then(function(modal){
+// 			$scope.modal = modal;
+// 			$scope.modal = modal.show();
+// 		});
+// 	}
+// });
 
-		console.log("zjeufjze");
-		
-		$ionicModal.fromTemplateUrl('src/nav/modal/more-modal.html',{
-			scope: $scope,
-			animation: 'slide-in-up'
-		}).then(function(modal){
-			$scope.modal = modal;
-			$scope.modal = modal.show();
-		});
-	}
+angular.module('places')
+.controller('ModalCtrl', function($scope, $ionicModal) {
+  $ionicModal.fromTemplateUrl('src/nav/modal/more-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
 });
